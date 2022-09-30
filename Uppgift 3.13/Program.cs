50be90f31fc255dcc227b006f4c6e885a155944a
GustavJohansson420 <gujo04002@utb.vaxjo.se>
2022-09-30 10:37:45 +0200

    Add project files.

---
+++ Uppgift 3.13/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Uppgift_3_13
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("Hur gammal är du?(år)");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if (ålder >= 16 && ålder <= 19)
+            {
+                Console.WriteLine("Du får delta i tävlingen");
+            }
+            else
+            {
+                Console.WriteLine("Du får inte delta i tävlingen");
+            }
+       
+        }
+    }
+}
